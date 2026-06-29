4dfd34ef1f7d37764070ccf5ad29aa4d901df2db
Kavyamuthuselvam <kavyamuthuselvam03@gmail.com>
2026-06-29 18:51:18 +0530

    Add files via upload

---
+++ xyloop.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+class Main{
+public static void main(String[] args) {
+    Scanner scan= new Scanner(System.in);
+    int x= scan.nextInt();
+   int y=scan.nextInt(); 
+    for(x=1;x<=y;x++){
+     System.out.println("x");
+     
+      
+    }
+       
+    
+    
+}
+}
