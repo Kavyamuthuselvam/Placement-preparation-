bd122dd4c5594ea81e1a136f7c319ac11acd42de
Kavyamuthuselvam <kavyamuthuselvam03@gmail.com>
2026-06-29 18:21:43 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class Main{
+public static void main(String[] args) {
+    Scanner scan= new Scanner(System.in);
+    int num = scan.nextInt();
+    char ch= scan.next().charAt(0);
+    String str=scan.nextLine();
+    System.out.println(num);
+    System.out.println(ch);
+    System.out.println(str);
+    
+}
+}
